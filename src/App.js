23697b961a2a3f8ae6e6bf9c58a5e38829a679d6
Oleg-Vasylkovan <vasylkovan@gmail.com>
2020-10-28 14:48:10 -0500

    cookie

--- src/App.js
+++ src/App.js
@@ -6,17 +6,18 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>I haven't delete this logo coz i feel it like it's nice to have it and makes my to do list look pretty LOL</p>
+        <h1>
+          Hello Dojo!
+        </h1>
+        <p>Things i need to do:</p>
+        <ul style={{listStyleType:"square"}}>
+          <li>Learn React</li>
+          <li>Climb Mt.Everest</li>
+          <li>Run a marathon</li>
+          <li>Feed a dogs</li>
+        </ul>
+        
       </header>
     </div>
   );
